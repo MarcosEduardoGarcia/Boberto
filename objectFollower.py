af882b374dc9a848ef9ece0f8fa0fb3717511286
MarcosEduardoGarcia <A01276213@itesm.mx>
2022-05-06 15:37:19 -0500

    Improves objecto Follower

--- objectFollower.py
+++ objectFollower.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python 
-import rospy 
+#!/usr/bin/env python
+import rospy
 import numpy as np
 from sensor_msgs.msg import Image
 from geometry_msgs.msg import Twist
@@ -11,21 +11,21 @@ class objectFollower():
         This class will suscribe to a topic with an Image and
         process the same image to find the colour Red and Green
     '''
-    def __init__(self): 
+    def __init__(self):
         # Callback fuction when the node is turned off
-        rospy.on_shutdown(self.cleanup) 
-        ############ CONSTANTS ################ 
+        rospy.on_shutdown(self.cleanup)
+        ############ CONSTANTS ################
         rate = 10
         vel_msg = Twist()
         self.image_received = 0
-        ###******* INIT PUBLISHERS *******### 
+        ###******* INIT PUBLISHERS *******###
         self.pub_image = rospy.Publisher('object_finder', Image, queue_size=1)
         self.pub_vel = rospy.Publisher('cmd_vel', Twist, queue_size=1)
-        ###******* INIT SUSCRIBERS *******### 
+        ###******* INIT SUSCRIBERS *******###
         self.bridge = CvBridge()
         rospy.Subscriber('video_source/raw',Image,self.image_cb)
-        #********** INIT NODE **********### 
-        r = rospy.Rate(rate) #1Hz 
+        #********** INIT NODE **********###
+        r = rospy.Rate(rate) #1Hz
         print("Node initialized " + str(rate)+ " hz")
         while not rospy.is_shutdown():
             if self.image_received:
@@ -34,12 +34,12 @@ class objectFollower():
                 main_image = self.cv_image.copy()
                 hue_image = cv.cvtColor(main_image, cv.COLOR_BGR2HSV)
                 result = main_image.copy()
-                
+
                 # lower boundary YELLOW color mask
                 lower_yellow = np.array([24, 97, 77])
                 upper_yellow = np.array([38, 255, 255])
                 main_mask_yellow = cv.inRange(hue_image, lower_yellow, upper_yellow)
-                
+
                 result_yellow = cv.bitwise_and(result,result, mask=main_mask_yellow)
                 ret,thresh_yellow = cv.threshold(main_mask_yellow, 200, 255, cv.THRESH_BINARY_INV)
                 # Simple blob detector
@@ -68,20 +68,31 @@ class objectFollower():
                 theta = center_width - main_xcord
                 vel = main_size
                 ang = theta/center_width
-                if vel > 0.5: vel = 0.5
+                if vel > 0.2: vel = 0.2
+                if abs(ang) > 0.1:
+                    if ang > 0 :
+                        ang = 0.1
+                    else:
+                        ang = -0.1
+
                 if main_size == 0:
                     print("No Object Detected")
                     vel_msg.linear.x = 0
-                    vel_msg.angular.z = 0
+                    vel_msg.angular.z = 0.1
                 else:
-                    vel_msg.angular.z = ang
-                    if main_size > 0.1:
-                        print("Velocidad Lineal: " + str(vel))
-                        print("Velocidad Angular: " + str(ang))
-                        vel_msg.linear.x = vel
+                    if abs(theta) < 100:
+                        vel_msg.angular.z = 0
+                        if main_size > 0.25:
+                            #print("Velocidad Lineal: " + str(vel))
+                            #print("Velocidad Angular: " + str(ang))
+                            vel_msg.linear.x = vel
+                        else:
+                            print("Object Reached")
+                            print("===============================")
+                            vel_msg.linear.x = 0
+                            vel_msg.angular.z = 0
                     else:
-                        print("Object Reached")
-                        print("===============================")
+                        vel_msg.angular.z = ang
                         vel_msg.linear.x = 0
                 # 300 valor del blob
                 self.pub_vel.publish(vel_msg)
@@ -93,13 +104,13 @@ class objectFollower():
     def image_cb(self, data):
         if not self.image_received:
             try:
-                #print("received ROS image, I will convert it to opencv") 
-                # We select bgr8 because it is the OpenCV encoding by default 
+                #print("received ROS image, I will convert it to opencv")
+                # We select bgr8 because it is the OpenCV encoding by default
                 self.cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
                 self.cv_width = data.width
-                self.image_received = 1 #Turn the flag on 
-            except CvBridgeError as e: 
-                print(e) 
+                self.image_received = 1 #Turn the flag on
+            except CvBridgeError as e:
+                print(e)
     def go_stop(self):
         # Detener
         stop = Twist()
@@ -107,9 +118,9 @@ class objectFollower():
     def cleanup(self):
         self.go_stop()
         print("\n-----FINALIZADO-----\n")
-############################### MAIN PROGRAM #################################### 
-if __name__ == "__main__": 
-    rospy.init_node("colour_filter", anonymous=True) 
+############################### MAIN PROGRAM ####################################
+if __name__ == "__main__":
+    rospy.init_node("colour_filter", anonymous=True)
     try:
         objectFollower()
     except rospy.ROSInterruptException:
